d7cf4bfd1c454d2dec189635402d70fb5a66942e
Kevin Shan <kevinshan2017@gmail.com>
2026-03-10 18:41:56 -0400

    placeholders and loader fallback

--- with-supabase-app/app/page.tsx
+++ with-supabase-app/app/page.tsx
@@ -1,31 +1,11 @@
-import { createClient } from "@/lib/supabase/server";
+import { Suspense } from "react";
+import Loader from "@/components/loader";
+import Home from "./page-dynamic";
 
-export default async function Home() {
-  const supabase = await createClient();
-
-  const { data, error } = await supabase
-    .from("Club")
-    .select("name");
-
-  if (error) {
-    console.error(error);
-    return <p>Error with loading the clubs</p>;
-  }
-
-  return (
-    <div>
-      <h1>First Club</h1>
-      {/* Display first clubs Ex. */}
-      <h1>Name: {data?.[0]?.name}</h1>
-      
-      {/* Display all clubs Ex. */}
-      {/* <ul className="space-y-2">
-        {data?.map((club, index) => (
-          <li key={index} className="border p-2 rounded">
-            {club.name}
-          </li>
-        ))}
-      </ul> */}
-    </div>
-  );
+export default async function Page() {
+    return (
+        <Suspense fallback={<Loader />}>
+            <Home />
+        </Suspense>
+    )
 }
