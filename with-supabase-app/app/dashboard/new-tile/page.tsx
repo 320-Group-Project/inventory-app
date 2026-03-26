3bbe09a195659d14377b5c63bd287b0a7cbbd6f9
rachel7martin <rlmartin@umass.edu>
2026-03-26 11:08:19 -0400

    Nav bar (#20)
    * started component
    
    * registered daisyui plugin. made navbar with button(no link) and added to new tile page
    
    * registered daisyui theme

--- with-supabase-app/app/dashboard/new-tile/page.tsx
+++ with-supabase-app/app/dashboard/new-tile/page.tsx
@@ -1,18 +1,20 @@
+import Navbar from "@/components/ui/navbar"
 export default function Page() {
-  return (
-    <><div data-theme="ULight" className="flex flex-col items-left justify-center gap-4 p-8">
-      <h1 className="text-3xl font-bold">New Tile</h1>
-      <line className="border-t" />
-      <div className="card-xl bg-base-100 w-full shadow-lg">
-        <div className="card-body items-left text-left">
-          <h2 className="card-title">Tile Name</h2>
-          <textarea className="textarea w-full" placeholder="Type here"></textarea>
-          <h2 className="card-title">Add Members</h2>
-          <textarea className="textarea w-full" placeholder="Member emails separated by &quot;,&quot;"></textarea>
-          <button className="btn w-12 bg-black text-white">Save</button>
-        </div>
-      </div>
-    </div></>
+    return (
+        <><Navbar />
+        <div className="flex flex-col items-left justify-center gap-4 p-8">
+            <h1 className="text-3xl font-bold">New Tile</h1>
+            <line className="border-t" />
+            <div className="card-xl bg-base-100 w-full shadow-lg">
+                <div className="card-body items-left text-left">
+                    <h2 className="card-title">Tile Name</h2>
+                    <textarea className="textarea w-full" placeholder="Type here"></textarea>
+                    <h2 className="card-title">Add Members</h2>
+                    <textarea className="textarea w-full" placeholder="Member emails separated by &quot;,&quot;"></textarea>
+                    <button className="btn w-12 bg-black text-white">Save</button>
+                </div>
+            </div>
+        </div></>
 
     )
 }
---
+++ with-supabase-app/components/ui/navbar.tsx
@@ -0,0 +1,10 @@
+"use client"
+const Navbar = () => {
+  return (
+    <div className="navbar bg-base-100 shadow-sm">
+      <a className="btn btn-ghost text-xl"><span className="text-primary">U</span>Inventory</a>
+    </div>
+  )
+}
+
+export default Navbar
