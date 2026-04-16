0173af7fb2eaebbc2e137d3a9e4cd770c4684397
mjloyall-UMA <mjloyall@umass.edu>
2026-04-16 10:12:43 -0400

    Add Back and PageTitle linking (#36)
    * Added linking to the back button and page title
    
    * Fix issues with current PageTitle objects not having hrefs

--- with-supabase-app/components/ui/back.tsx
+++ with-supabase-app/components/ui/back.tsx
@@ -1,8 +1,21 @@
 "use client"
-const Back = () => {
-  return (
+import Link from "next/link"
+
+interface BackProps {
+  href?: string
+}
+
+const Back = ({href}: BackProps) => {
+  const Icon = (
     <svg width="28" height="28"className="translate-y-1" id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 122.88 108.06"><title>back-arrow</title><path d="M63.94,24.28a14.28,14.28,0,0,0-20.36-20L4.1,44.42a14.27,14.27,0,0,0,0,20l38.69,39.35a14.27,14.27,0,0,0,20.35-20L48.06,68.41l60.66-.29a14.27,14.27,0,1,0-.23-28.54l-59.85.28,15.3-15.58Z"/></svg>
   )
+
+  if(href) return (
+    <Link href={href}>
+      {Icon}
+    </Link>
+  )
+  return <>{Icon}</>
 }
 
 export default Back
