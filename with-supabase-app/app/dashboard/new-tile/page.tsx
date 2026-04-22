77cc7452f736358d119a9f41e75860ab0c56fb21
walterlloyd <wjpl.dev@gmail.com>
2026-04-22 00:02:30 -0400

    added more navigation function

--- with-supabase-app/app/dashboard/new-tile/page.tsx
+++ with-supabase-app/app/dashboard/new-tile/page.tsx
@@ -4,7 +4,7 @@ export default function Page() {
     return (
         <><Navbar />
         <div className="flex flex-col items-left justify-center gap-4 p-8">
-            <PageTitle title="New Tile" />
+            <PageTitle title="New Tile" href="/dashboard" />
             <line className="border-t" />
             <div className="card-xl bg-base-100 w-full shadow-lg">
                 <div className="card-body items-left text-left">
@@ -12,7 +12,9 @@ export default function Page() {
                     <textarea className="textarea w-full" placeholder="Type here"></textarea>
                     <h2 className="card-title">Add Members</h2>
                     <textarea className="textarea w-full" placeholder="Member emails separated by &quot;,&quot;"></textarea>
+                    <a href="/dashboard">
                     <button className="btn w-12 bg-black text-white">Save</button>
+                    </a>
                 </div>
             </div>
         </div></>
