2fbd944571c11825f04987547e7d3d0484791097
walterlloyd <wjpl.dev@gmail.com>
2026-04-23 10:54:08 -0400

    update

--- with-supabase-app/app/clubs/category/new/page.tsx
+++ with-supabase-app/app/clubs/category/new/page.tsx
@@ -2,15 +2,17 @@
 import { NewCategoryContent } from "@/components/new-category";
 import Back from "@/components/ui/back";
 import { useSearchParams } from "next/navigation";
+import { useRouter } from "next/navigation";
 
 export default function NewCategoryPage() {
-  const searchParams = useSearchParams();
-  const backHref = searchParams.get("from") || "/dashboard";
+  const router = useRouter();
   return (
     <div className="min-h-screen bg-base-100 text-base-content p-8 md:p-12">
       <div className="max-w-5xl mx-auto">
         <button className="btn btn-ghost btn-circle hover:bg-base-200 mb-2">
-          <Back href={backHref}/>
+        <button onClick={() => router.back()}>
+          <Back />
+        </button>
         </button> 
         <h1 className="text-4xl font-normal mb-2 mt-2">New Category</h1>
         <hr className="border-secondary border-t-2 mb-10 w-full" />
