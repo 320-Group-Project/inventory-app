2ccba01279d8686b2d4b04d516515e56d40aafdf
Chloe Le <133743874+its-cle@users.noreply.github.com>
2026-05-07 10:46:10 -0400

    New item copy page (#83)
    * Created New Item Copy Page
    
    * Fixed Condition and Available selection ui delays
    
    * fix build error
    
    ---------
    
    Co-authored-by: Chloe Le <cle@wyebot.com>

--- with-supabase-app/app/clubs/category/[categoryId]/item/new/page.tsx
+++ with-supabase-app/app/clubs/category/[categoryId]/item/new/page.tsx
@@ -1,14 +1,12 @@
 import { Suspense } from "react";
 import { NewItemForm } from "@/components/new-item-form";
-import Navbar from "@/components/ui/navbar";
 
 export default function NewItemPage() {
   return (
-    <><Navbar />
     <div className="min-h-screen bg-base-100 text-base-content p-4 md:p-8 flex items-center justify-center">
       <Suspense fallback={<div className="text-xl">Loading form...</div>}>
         <NewItemForm className="w-full" />
       </Suspense>
-    </div></>
+    </div>
   );
 }
